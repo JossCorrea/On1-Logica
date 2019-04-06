02e0b93a15d95223cfd578eae8e0d7b7e537eb4e
barbaraduarte <barbara.silveira@live.com>
2019-04-05 22:52:20 -0300

    exercicios logica aula1

--- exercicios-para-casa/ex01.js
+++ exercicios-para-casa/ex01.js
@@ -12,52 +12,59 @@
 // Tá com dúvidas sobre como responder ao exercício? Leia o arquivo _Intrucoes.md
 
 // Crie/declare uma variável para guardar o nome do curso que você está fazendo na reprograma (sem valor).
-[Substitua este texto por sua resposta]
+var frontEndRep 
 
 // Mostre o valor atual de sua variável no console 
-[Substitua este texto por sua resposta]
+console.log(frontEndRep)
 
 // Atribua um valor à variável que você acabou de criar
-[Substitua este texto por sua resposta]
+var frontEndRep = 2019
+var num = 2019
 
 // Agora mostre novamente o valor atual de sua variável no console 
-[Substitua este texto por sua resposta]
+console.log(frontEndRep)
 
-// Crie/declare uma variável para guardar uma nota de 1 a 10 (sem valor).
-[Substitua este texto por sua resposta]
+// Crie/declare uma variável para guardar uma nota de 1 a 10 (sem valor)
+var notaLogJs
+var nota = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // Nesta variável atribua o valor da nota que você daria (hoje) para os seus conhecimentos de lógica (seja realista sem ser cruel com você mesma, lembre que você está começando)
-[Substitua este texto por sua resposta]
+var notaLogJs = 4
+
 
 // Crie/declare uma nova variável chamada 'soma' e atribua o resultado da soma de dois valores nesta variável (tente fazer tudo em uma linha) 
-[Substitua este texto por sua resposta]
+var soma = notaLogJs + frontEndRep
 
 // Incremente (some mais 1) à sua variável 'soma' usando 3 formas diferentes (dica: pesquise operadores unários) 
-[Substitua este texto por sua resposta]
+soma =+ 1;
+soma = soma + 1;
+soma++;
 
 // Crie/declare uma variável 'multiplicacao' que multiplica a sua variável 'soma' por um valor qualquer
-[Substitua este texto por sua resposta]
+const multiplicacao = soma * 3
 
 // Mostre o valor das variáveis 'soma' e 'multiplicacao' usando três formas diferentes 
-[Substitua este texto por sua resposta]
+
 
 // Crie/declare uma variável chamada 'queroSerDesenvolvedora' e atribua um valor booleano à ela (...tô torcendo pra ser true..rsrsrsr)
-[Substitua este texto por sua resposta]
+var queroSerDesenvolvedora = true
+ 
 
 // Crie/Declare uma variável chamada 'sanduba' do tipo array e acrescente valores correspondentes àos ingretientes da sua receita preferida. (Nós vimos array correndo então vou dar uma dica aqui: var sanduba = ['pao', 'ovo', 'queijo'] )
-[Substitua este texto por sua resposta]
+var sanduba = ['pao', 'ovo', 'queijo','maionese', 'salada']
 
 // Moste a variável 'sanduba' no console
-[Substitua este texto por sua resposta]
+console.log(sanduba)
 
 // Mostre o primeiro igrediente de 'sanduba' (desafio pra você pesquisar)
-[Substitua este texto por sua resposta]
+var ingrediente1 = sanduba [0]
+console.log (sanduba[0])
 
 // Acrescente um ingrediente ao seu 'sanduba' (desafio pra você pesquisar)
-[Substitua este texto por sua resposta]
+var acrescentarIngred = sanduba.push('salada')
 
 // Substitua um ingrediente do seu 'sanduba' (desafio pra você pesquisar...sempre tento trocar a ervilha, quando tem)
-[Substitua este texto por sua resposta]
+sanduba[0] = 'ovo'
 
 // Escolha 3 variáveis que você criou e mostre o seus respectivos tipos (lembre que tem um comando pra isso)
-[Substitua este texto por sua resposta]
+
--- exercicios-para-casa/ex07.js
+++ exercicios-para-casa/ex07.js
@@ -1,14 +1,19 @@
 // Estrutura for
 
 // Faça um algoritmo que mostre os números de 0 até 100 
-[Substitua este texto por sua resposta]
+for (let i=0; i<=100; i++){
+    console.log(i)
+}
 
 // Agora faça uma versão que conta de 0 até 100 de 5 em 5 números
-[Substitua este texto por sua resposta]
+for (let i=0; i<=100; i=i+5){
+    console.log(i)
+}
 
 // Faça um algoritmo que conte de 60 até 0 de trás para frente
-[Substitua este texto por sua resposta]
-
+for (let i=60; i>=0; i--){
+    console.log(i)
+}
 
 // [DESAFIO - NÃO OBRIGATÓRIO] 
 // Crie um programa gerador de estrelas (não vale usar métodos, tem que ser na força bruta...rsrsr) 
@@ -20,4 +25,11 @@
 // ****
 // *****
 // Dica: O operador + é só pra somar ?
-[Substitua este texto por sua resposta]
+
+let numeroEstrelas = parseInt(prompt('Informe o numero de estrelas'));
+
+let estrelas = '';
+for (i=1; i<=numeroEstrelas; i++){
+    estrelas += '*'
+    console.log(estrelas)
+}
